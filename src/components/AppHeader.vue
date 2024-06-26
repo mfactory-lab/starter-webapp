7ee4edb08d744c37321b53c19ccd770b9b30a2f1
garniy <forbedlam@gmail.com>
2024-06-26 19:38:25 +0300

    fix: menu icon, add menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,40 @@
+<script setup lang="ts">
+import { matMenu } from '@quasar/extras/material-icons'
+</script>
+
 <template>
   <header class="container">
     <q-toolbar class="q-my-md">
-      <q-btn flat round dense icon="menu-outline" class="q-mr-sm" />
+      <q-btn flat round dense :icon="matMenu" class="q-mr-sm">
+        <q-menu fit>
+          <q-list style="min-width: 100px">
+            <q-item clickable>
+              <q-item-section>New tab</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section>New incognito tab</q-item-section>
+            </q-item>
+            <q-separator />
+            <q-item clickable>
+              <q-item-section>Recent tabs</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section>History</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section>Downloads</q-item-section>
+            </q-item>
+            <q-separator />
+            <q-item clickable>
+              <q-item-section>Settings</q-item-section>
+            </q-item>
+            <q-separator />
+            <q-item clickable>
+              <q-item-section>Help &amp; Feedback</q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </q-btn>
 
       <q-space />
 
