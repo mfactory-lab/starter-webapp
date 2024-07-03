23cde1b04f6e264094893f18fe137d34fe78f86c
tiamo <vk.tiamo@gmail.com>
2024-07-04 00:17:03 +0300

    fix: support quasar ssr

--- src/modules/quasar.ts
+++ src/modules/quasar.ts
@@ -1,14 +1,21 @@
 import type { QuasarPluginOptions } from 'quasar'
 import { Dark, Dialog, Loading, LocalStorage, Notify, Quasar } from 'quasar'
-import iconSet from 'quasar/icon-set/svg-eva-icons.js'
+import iconSet from 'quasar/icon-set/svg-eva-icons.js?inline'
 import type { UserModule } from '~/types'
 
-export const install: UserModule = ({ isClient, app }) => {
-  if (!isClient) {
-    return
+export const install: UserModule = (ctx) => {
+  const ssrContext = {
+    req: {
+      url: ctx.router.currentRoute.value.path,
+      headers: {},
+    },
   }
 
-  app.use(Quasar, {
+  ctx.app.use(Quasar, {
+    config: {
+      ripple: false,
+      // dark: true,
+    },
     plugins: {
       Dialog,
       Notify,
@@ -17,5 +24,8 @@ export const install: UserModule = ({ isClient, app }) => {
       Loading,
     },
     iconSet,
-  } as QuasarPluginOptions)
+  } as QuasarPluginOptions,
+  // @ts-expect-error ssrContext is valid here
+  ssrContext,
+  )
 }
